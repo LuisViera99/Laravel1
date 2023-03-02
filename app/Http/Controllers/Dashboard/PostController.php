a46ff09bea098f4d6ead73a08d85e63718cd4013
LuisViera99 <luis.viera@cceo.com.mx>
2023-03-02 10:17:04 -0600

    Reglas de Validacion

--- app/Http/Controllers/Dashboard/PostController.php
+++ app/Http/Controllers/Dashboard/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Post\StoreRequest;
 use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
@@ -34,10 +35,10 @@ class PostController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreRequest $request)
     {
         $data = array_merge($request->all(),['image' => '']);
-      // dd($data);
+      dd($data);
        Post::create($data);
         //echo $request -> input('slug');
     }
